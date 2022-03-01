02caba846256d47419390bf20299690224fcce07
mattreee <fantinatomatheuss@gmail.com>
2022-03-01 11:53:55 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+	return <div>teste</div>;
 }
 
 export default App;
