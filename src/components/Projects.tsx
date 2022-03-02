01f247d9500c4ca57bf9cf0bbec29b40b4461135
mattreee <fantinatomatheuss@gmail.com>
2022-03-02 19:01:45 -0400

    project images added, hover effects halfway finished

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,37 +3,67 @@ const Projects = () => {
 		<section className="projects">
 			<h2 className="projects__title">Projects</h2>
 			<div className="projects__cards">
-				<div className="projects__card">
+				<div className="projects__card beach">
 					<p className="projects__type">App</p>
-					<p className="projects__name">Plant Manager</p>
-					<p className="projects__desc">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					</p>
+					<div className="projects__info">
+						<p className="projects__name">Plant Manager</p>
+						<p className="projects__desc">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et neque
+							tenetur animi reiciendis necessitatibus inventore accusantium
+							deleniti autem, modi, totam dicta delectus quam!
+						</p>
+					</div>
 				</div>
-				<div>
-					<p>Web system</p>
-					<p>RPG Sheet</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<div className="projects__card camera">
+					<p className="projects__type">Web system</p>
+					<div className="projects__info">
+						<p className="projects__name">RPG Sheet</p>
+						<p className="projects__desc">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+							eius magnam temporibus!
+						</p>
+					</div>
 				</div>
-				<div>
-					<p>App</p>
-					<p>Plant Manager</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<div className="projects__card coffee">
+					<p className="projects__type">App</p>
+					<div className="projects__info">
+						<p className="projects__name">Plant Manager</p>
+						<p className="projects__desc">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+							molestiae dignissimos aut accusamus.
+						</p>
+					</div>
 				</div>
-				<div>
-					<p>Web system</p>
-					<p>RPG Sheet</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<div className="projects__card jump">
+					<p className="projects__type">Web system</p>
+					<div className="projects__info">
+						<p className="projects__name">RPG Sheet</p>
+						<p className="projects__desc">
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+							Asperiores dolores explicabo, nesciunt placeat eaque officia.
+						</p>
+					</div>
 				</div>
-				<div>
-					<p>App</p>
-					<p>Plant Manager</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<div className="projects__card path">
+					<p className="projects__type">App</p>
+					<div className="projects__info">
+						<p className="projects__name">Plant Manager</p>
+						<p className="projects__desc">
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem
+							ipsam vero excepturi.
+						</p>
+					</div>
 				</div>
-				<div>
-					<p>App</p>
-					<p>Plant Manager</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<div className="projects__card starry">
+					<p className="projects__type">App</p>
+					<div className="projects__info">
+						<p className="projects__name">Plant Manager</p>
+						<p className="projects__desc">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+							suscipit at nostrum? Quibusdam tenetur voluptatum obcaecati
+							laborum?
+						</p>
+					</div>
 				</div>
 			</div>
 		</section>
