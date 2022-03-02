12573d5f084620ad7a2bd1e72cbe25ebacaba64a
mattreee <fantinatomatheuss@gmail.com>
2022-03-02 13:37:33 -0400

    changed name and moved overflow from body to intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
 	return (
 		<section className="intro">
 			<p className="intro__hi">Hello, I'm</p>
-			<h1 className="intro__name">Nome Sobrenome</h1>
+			<h1 className="intro__name">namee lastnaame</h1>
 			<p className="intro__desc">
 				<span className="bold-text">
 					Lorem ipsum dolor sit amet consectetur
