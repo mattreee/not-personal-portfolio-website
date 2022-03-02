6bc57ab936617e416e424e2aa3b89171368330fc
mattreee <fantinatomatheuss@gmail.com>
2022-03-02 15:54:05 -0400

    finished services

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,40 @@
 const Projects = () => {
 	return (
-		<section>
-			<div>
-				<p>App</p>
-				<p>Plant Manager</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+		<section className="projects">
+			<h2 className="projects__title">Projects</h2>
+			<div className="projects__cards">
+				<div className="projects__card">
+					<p className="projects__type">App</p>
+					<p className="projects__name">Plant Manager</p>
+					<p className="projects__desc">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					</p>
+				</div>
+				<div>
+					<p>Web system</p>
+					<p>RPG Sheet</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
+				<div>
+					<p>App</p>
+					<p>Plant Manager</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
+				<div>
+					<p>Web system</p>
+					<p>RPG Sheet</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
+				<div>
+					<p>App</p>
+					<p>Plant Manager</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
+				<div>
+					<p>App</p>
+					<p>Plant Manager</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
 			</div>
 		</section>
 	);
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,11 +1,24 @@
 const Services = () => {
 	return (
-		<section>
-			<ul>
-				<li>Development of web apps</li>
-				<li>Development of Rest APIs</li>
-				<li>Developmento of mobile apps</li>
-				<li>Software tests</li>
+		<section className="services">
+			<h2 className="services__title">services</h2>
+			<ul className="services__ul">
+				<li className="services__li">
+					<span className="bullet">&#8226;</span>
+					<p className="services__li-text">Web apps development</p>
+				</li>
+				<li className="services__li">
+					<span className="bullet">&#8226;</span>
+					<p className="services__li-text">Development of mobile apps</p>
+				</li>
+				<li className="services__li">
+					<span className="bullet">&#8226;</span>
+					<p className="services__li-text">Rest APIs development</p>
+				</li>
+				<li className="services__li">
+					<span className="bullet">&#8226;</span>
+					<p className="services__li-text">Software tests</p>
+				</li>
 			</ul>
 		</section>
 	);
