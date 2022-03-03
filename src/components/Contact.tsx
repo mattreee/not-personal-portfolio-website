70e5623a731b47bb3b1699c7c69bdc39ae969420
mattreee <fantinatomatheuss@gmail.com>
2022-03-03 16:56:25 -0400

    finished contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,32 +1,67 @@
 const Contact = () => {
 	return (
-		<section>
-			<h2>Do you wanna talk?</h2>
-			<p>username@email.com</p>
-			<form>
-				<label htmlFor="name">Name</label>
-				<input type="text" id="name" name="name" placeholder="Full name" />
+		<section className="contact">
+			<div className="contact__intro-container">
+				<h2 className="contact__title">Do you wanna talk?</h2>
+				<div className="contact__title-bar">a</div>
+				<p className="contact__email">username@email.com</p>
+			</div>
+			<div className="contact__wrapper">
+				<form className="contact__form">
+					<div className="contact__form-item">
+						<label className="contact__form-label" htmlFor="name">
+							Name
+						</label>
+						<input
+							className="contact__form-text-input"
+							type="text"
+							id="name"
+							name="name"
+							placeholder="Full name"
+						/>
+					</div>
 
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					id="email"
-					name="email"
-					placeholder="username@email.com"
-				/>
+					<div className="contact__form-item">
+						<label className="contact__form-label" htmlFor="email">
+							Email
+						</label>
+						<input
+							className="contact__form-text-input"
+							type="email"
+							id="email"
+							name="email"
+							placeholder="username@email.com"
+						/>
+					</div>
 
-				<label htmlFor="howhere">How did you get here?</label>
-				<input
-					type="text"
-					id="howhere"
-					name="howhere"
-					placeholder="Indicated, social media..."
-				/>
-				<label htmlFor="message">Message</label>
-				<textarea name="message" id="message"></textarea>
+					<div className="contact__form-item">
+						<label className="contact__form-label" htmlFor="howhere">
+							How did you get here?
+						</label>
+						<input
+							className="contact__form-text-input"
+							type="text"
+							id="howhere"
+							name="howhere"
+							placeholder="Indicated, social media..."
+						/>
+					</div>
 
-				<input type="submit" value="Send" />
-			</form>
+					<div className="contact__form-item">
+						<label className="contact__form-label" htmlFor="message">
+							Message
+						</label>
+						<textarea
+							className="contact__form-text-input"
+							name="message"
+							id="message"
+							placeholder="hello, I'm..."
+						></textarea>
+					</div>
+
+					<input className="contact__submit" type="submit" value="Send" />
+				</form>
+			</div>
 		</section>
 	);
 };
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,11 +7,13 @@ const Experience = () => {
 			</div>
 			<div className="experience__timeline-container">
 				<div className="experience__timeline-item">
-					<p>jan. 9999</p>
-					<div>
-						<h3>Mobile Developer</h3>
-						<p>FootTalk - sep 9999 - now</p>
-						<p>
+					<p className="experience__date">jan. 9999</p>
+					<div className="experience__text">
+						<h3 className="experience__text-title">Mobile Developer</h3>
+						<p className="experience__text-date">
+							FootTalk &#8226; sep 9999 - now
+						</p>
+						<p className="experience__text-desc">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
 							officiis quis rerum dignissimos eaque, ex magni earum, sint illum
 							aperiam consectetur suscipit nihil fugiat officia?
@@ -19,11 +21,13 @@ const Experience = () => {
 					</div>
 				</div>
 				<div className="experience__timeline-item">
-					<p>feb. 9998</p>
-					<div>
-						<h3>Test analyst</h3>
-						<p>Dellete Lead - febr 7384 - now</p>
-						<p>
+					<p className="experience__date">feb. 9998</p>
+					<div className="experience__text">
+						<h3 className="experience__text-title">Test analyst</h3>
+						<p className="experience__text-date">
+							Dellete Lead &#8226; feb 7384 - now
+						</p>
+						<p className="experience__text-desc">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
 							voluptate similique molestias eveniet quos repellendus corrupti
 							eaque?
